8e6e5432ab8e6d3865abca10e5f8aada76732337
Ron Lavit <lavitr@yandex.ru>
2024-10-18 22:56:59 +0200

    eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/prop-types": 0,
+    "no-class-assign": 0
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
